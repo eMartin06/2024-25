aa54ab041b07445162cfe71951666234020a1b28
Martin <erdei.martin.2021e@ms.gdszeged.hu>
2024-10-04 09:15:32 +0200

    word

---
+++ Kampi/Feladatforciklus/Feladatforciklus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Feladatforciklus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float összeg = 0;
+            float min = float.MaxValue;
+
+            for (int i = 0; i < 5; i++)
+            {
+            Console.WriteLine("Add meg egy számot!");
+                float szám = float.Parse(Console.ReadLine());
+                összeg += szám;
+           
+                if (min > szám)
+                {
+                    min = szám;
+                }
+            }
+            Console.WriteLine($"A számok összege: {összeg}, és az átlaga: {összeg/5}");
+            Console.WriteLine($"A legkisebb szám a listánól: {min}");
+        }
+    }
+}
