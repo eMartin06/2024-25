8397a107582719f0f4e23159ab98018cae24133d
Martin <erdei.martin.2021e@ms.gdszeged.hu>
2024-11-06 13:28:33 +0100

    0

---
+++ Kampi/file olvasás város/file olvasás város/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace file_olvasás_város
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int összeg = 0;
+            StreamReader lista = new StreamReader("adatok.txt", Encoding.UTF8);
+            Console.Write("Add meg a cél várost (Szeged,Pest,Dóc). ");
+            string város = Console.ReadLine();
+            while (!lista.EndOfStream)
+            {
+                string[] tartalom = lista.ReadLine().Split();
+                if (tartalom[1]== város)
+                {
+                    összeg += int.Parse(tartalom[2]);
+                }
+                
+            }
+            Console.Write($"Annak a városnak tartozó emberek fizetésének összege: {összeg}");
+            lista.Close();
+            Console.ReadKey();
+
+        }
+
+    }
+}
